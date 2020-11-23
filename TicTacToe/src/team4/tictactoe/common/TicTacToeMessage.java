851dd010289a7b956a5765916d17b154a8839b77
zlaepek <69634389+zlaepek@users.noreply.github.com>
2020-11-23 22:59:16 +0900

    게임 코드 수정(병합 필요)

--- TicTacToe/src/team4/tictactoe/common/TicTacToeMessage.java
+++ TicTacToe/src/team4/tictactoe/common/TicTacToeMessage.java
@@ -1,70 +1,110 @@
 package team4.tictactoe.common;
 
 /**
+ * TicTacToe 게임 정보 송수신 메시지
+ * 
+ * 클라이언트가 자신의 클릭한 버튼의 행과 열, X 또는 O 표시를 서버에 전송한다.
+ * 서버는 현재 게임판을 보고 승패 여부를 판단하여 클라이언트에게 전송한다.
  * 
  * @author 
  * @since 2020.11
  */
 public class TicTacToeMessage implements Message {
-	
-	private static final String MSG_NAME = "TicTacToeMessage:";
-	private static final String ATTR_GAME_STATE = "gameState=";
 
-	public static final int GAME_STATE_UNDEFINED = -1;
-	public static final int GAME_STATE_WAITING = 0;
-	
-	/**
-	 * 게임 진행 상태
-	 * -1 : 미지정
-	 * 0 : 대기중
-	 * 1 : Player 1 승
-	 * 2 : Player 2 승
-	 * 3 : 비김
-	 * 4 : 진행중
-	 */
-	public int gameState;
-	
-	@Override
-	public void reset() {
-		gameState = GAME_STATE_UNDEFINED;
-		
-		//TODO 멤버변수 추가되면 해당 멤버변수 초기화
-	}
+   private static final String MSG_NAME = "TicTacToeMessage:";
+   private static final String ATTR_USER_MARK = "playerMark=";
+   private static final String ATTR_USER_ROW = "row=";
+   private static final String ATTR_USER_COLUMN = "column=";
+   private static final String ATTR_USER_TURN = "turn=";
+   public static final String GAME_STATE_WAITING = "gameState=";
+   /**
+    * O 또는 X.
+    */
+   public String playerMark;
 
-	@Override
-	public String getMessageString() {
-		StringBuffer msgBuf = new StringBuffer();
+   /**
+    * 보드판에 표시해야 할 행.
+    */
+   public String row;
 
-		msgBuf.append(MSG_NAME);
-		if (gameState!=-1) {
-			msgBuf.append(ATTR_GAME_STATE + gameState + ",");
-		}
-		
-		// TODO 멤버변수 추가되면 해당 멤버변수를 메시지에 추가
-		
-		return null;
-	}
+   /**
+    * 보드판에 표시해야 할 열.
+    */
+   public String column;
+   
+   /**
+    * 게임 순서를 알려준다.(O/X)
+    */
+   public String turn;
+   
+   /**
+    * 게임 상황를 알려준다.(winO/winX/Tie)
+    */
+   public String gameState;
 
-	@Override
-	public boolean setMessageString(String msg) {
-		if (!msg.startsWith(MSG_NAME)) {
-			return false;
-		}
+   @Override
+   public void reset() {
+      playerMark = null;
+      row = null;
+      column = null;
+      turn = null;
+      gameState = null;
+   }
+   
+   @Override
+   public String getMessageString() {
+      StringBuffer msgBuf = new StringBuffer();
 
-		reset();
-		String msgItems[] = msg.substring(MSG_NAME.length()).split(",");
-		String msgItem;
-		int idx;
-		for (idx=0; idx<msgItems.length; idx++) {
-			msgItem = msgItems[idx];
-			if (msgItem.startsWith(ATTR_GAME_STATE)) {
-				gameState = Integer.parseInt(msgItem.substring(ATTR_GAME_STATE.length()));
-			}
-			
+      msgBuf.append(MSG_NAME);
+      if (playerMark!=null && !playerMark.isEmpty()) {
+         msgBuf.append(ATTR_USER_MARK + playerMark + ",");
+      }
+      if (row!=null && !row.isEmpty()) {
+         msgBuf.append(ATTR_USER_ROW + row + ",");
+      }
+      if (column!=null && !column.isEmpty()) {
+         msgBuf.append(ATTR_USER_COLUMN + column + ",");
+      }
+      if (turn!=null && !turn.isEmpty()) {
+         msgBuf.append(ATTR_USER_TURN + turn + ",");
+      }
+      if (turn!=null && !turn.isEmpty()) {
+         msgBuf.append(GAME_STATE_WAITING + gameState + ",");
+      }
+      
+      return msgBuf.toString();
+   }
 
-			// TODO 멤버변수 추가되면 해당 멤버변수를 메시지에서 읽도록 if문 추가
-		}
+   @Override
+   public boolean setMessageString(String msg) {
+      if (!msg.startsWith(MSG_NAME)) {
+         return false;
+      }
 
-		return true;
-	}
+      reset();
+      
+      String msgItems[] = msg.substring(MSG_NAME.length()).split(",");
+      String msgItem;
+      int idx;
+      for (idx=0; idx<msgItems.length; idx++) {
+         msgItem = msgItems[idx];
+         if (msgItem.startsWith(ATTR_USER_MARK)) {
+            playerMark = msgItem.substring(ATTR_USER_MARK.length());
+         }
+         if (msgItem.startsWith(ATTR_USER_ROW)) {
+            row = msgItem.substring(ATTR_USER_ROW.length());
+         }
+         if (msgItem.startsWith(ATTR_USER_COLUMN)) {
+            column = msgItem.substring(ATTR_USER_COLUMN.length());
+         }
+         if (msgItem.startsWith(ATTR_USER_TURN)) {
+            turn = msgItem.substring(ATTR_USER_TURN.length());
+         }
+         if (msgItem.startsWith(GAME_STATE_WAITING)) {
+            turn = msgItem.substring(GAME_STATE_WAITING.length());
+         }
+      }
+
+      return true;
+   }
 }
--- TicTacToe/src/team4/tictactoe/game/TicTacToeGame.java
+++ TicTacToe/src/team4/tictactoe/game/TicTacToeGame.java
@@ -11,24 +11,129 @@ import team4.tictactoe.server.GameRoom;
  */
 public class TicTacToeGame {
 
-	/**
-	 * 게임방을 연결한다.
-	 */
-	public GameRoom gameRoom = null;
+   /**
+    * 게임방을 연결한다.
+    */
+   public GameRoom gameRoom = null;
 
-	public TicTacToeGame(GameRoom newGameRoom) {
-		gameRoom = newGameRoom;
-	}
+   public TicTacToeGame(GameRoom newGameRoom) {
+      gameRoom = newGameRoom;
+   }
 
-	public void processMessage(TicTacToeMessage msg) {
-		// TODO 게임 처리
-		
-		
-		// 게임 결과 보내기
-		TicTacToeMessage newMsg = new TicTacToeMessage();
-		newMsg.gameState = TicTacToeMessage.GAME_STATE_WAITING;
-		//TODO 추가 멤버변수 값 설정
-		gameRoom.player1.sendMessage(newMsg); // Player 1에게 메시지 전송
-		gameRoom.player2.sendMessage(newMsg); // Player 2에게 메시지 전송
-	}
+   
+   static char[][] board = new char[3][3];
+   //2차원 문자 배열을 이용하여서 보드를 나타낸다.
+   
+
+   TicTacToeGame() {
+        for (int row = 0; row < 3; row++)  {
+           for(int col = 0; col < 3; col++) {
+              board[row][col]= ' ';
+           }
+        }         
+   }//2차원 배열을 초기화한다.
+
+   public static void setBoard(int i, int j, char playerMark) {
+       board[i][j] = playerMark;
+   }
+   
+   public char getBoard(int i, int j) {
+      return board[i][j];
+   }
+   
+   public boolean checkWin() {
+      
+      boolean win=false;
+      
+      for(int i=0;i<3;i++){
+         if(board[i][0]==board[i][1]&&board[i][1]==board[i][2]){
+            win = true;
+          }
+      }
+      
+      for(int i=0;i<3;i++){
+         if(board[0][i]==board[1][i]&&board[1][i]==board[2][i]){
+            win = true;
+            }
+      }
+      
+      if(board[0][0]==board[1][1]&&board[1][1]==board[2][2]){
+         win=true;
+      }
+
+      if(board[0][2]==board[1][1]&&board[1][1]==board[2][0]){
+         win=true;
+      }
+         
+      return win;
+   }//이겼는지 판단
+   
+   public boolean checkTie() {
+      for (int row = 0; row < 3; row++)  {
+         for(int col = 0; col < 3; col++) {
+              if(board[row][col]== ' ') {
+                 return false;
+              }
+           }
+       }   
+      return true;
+   }//비겼는지 판단
+   
+   /**
+    * 승패 결과를 양쪽 클라이언트들에게 결과를 넘겨줌
+    * @param msg
+    */
+   public void processMessage(TicTacToeMessage msg) {
+      
+      char mark = msg.playerMark.charAt(0);
+      TicTacToeGame.setBoard(Integer.parseInt(msg.row), Integer.parseInt(msg.column), mark);
+      //사용자들의 정보를 보드판에 입력한다.
+      
+       // 게임 결과 보내기
+       TicTacToeMessage newMsg = new TicTacToeMessage();
+       newMsg.gameState = TicTacToeMessage.GAME_STATE_WAITING;
+       
+      if (checkWin()==true) {
+
+         if(msg.turn=="X") { // X플레이어가 이긴 상황
+            
+            newMsg.gameState = "winX";
+            
+             gameRoom.player1.sendMessage(newMsg); // Player 1에게 메시지 전송
+             gameRoom.player2.sendMessage(newMsg); // Player 2에게 메시지 전송
+         } 
+         else { // O플레이어가 이긴 상황
+
+            newMsg.gameState = "winO"; 
+            
+             gameRoom.player1.sendMessage(newMsg); // Player 1에게 메시지 전송
+             gameRoom.player2.sendMessage(newMsg); // Player 2에게 메시지 전송
+         }
+      } 
+      else if (checkTie()==true) { // 무승부인 상황
+
+         newMsg.gameState = "tie"; 
+
+          gameRoom.player1.sendMessage(newMsg); // Player 1에게 메시지 전송
+          gameRoom.player2.sendMessage(newMsg); // Player 2에게 메시지 전송
+      } 
+      else { //승부가 나지 않은 상황에서는 다음 차례로 넘어간다.
+
+         if(msg.turn == "X") {
+            newMsg.turn = "O";
+         }
+         else {
+            newMsg.turn = "X";
+         }
+          gameRoom.player1.sendMessage(newMsg); // Player 1에게 메시지 전송
+          gameRoom.player2.sendMessage(newMsg); // Player 2에게 메시지 전송
+      }
+       
+
+      
+
+   }
+
+
+   
 }
